62c5ff5836ec0d1ee2a5482f5f9fb0795be63f53
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-01-02 17:39:03 +0200

    .

--- 2023/day1.js
+++ 2023/day1.js
@@ -4,90 +4,90 @@ const input = fs.readFileSync('./2023/day1.txt', 'utf-8');
 const arr = input.split('\n').filter(Boolean);
 
 const numberMap = {
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
 };
 
 function isNumber(char) {
-    return !isNaN(parseInt(char));
+  return !isNaN(parseInt(char));
 }
 
 function firstNumber(string) {
-    let numberIndex = -1;
-    let num = 0;
-    let stringIndex = -1;
-    let str = 0;
+  let numberIndex = -1;
+  let num = 0;
+  let stringIndex = -1;
+  let str = 0;
 
-    for (let i = 0; i < string.length; i++) {
-        if (isNumber(string[i])) {
-            numberIndex = i;
-            num = parseInt(string[i]);
-            break;
-        }
+  for (let i = 0; i < string.length; i++) {
+    if (isNumber(string[i])) {
+      numberIndex = i;
+      num = parseInt(string[i]);
+      break;
     }
+  }
 
-    for (let i = 0; i < string.length; i++) {
-        for (const key of Object.keys(numberMap)) {
-            if (string.startsWith(key, i)) {
-                stringIndex = i;
-                str = numberMap[key];
-                break;
-            }
-        }
-        if (stringIndex != -1) break;
+  for (let i = 0; i < string.length; i++) {
+    for (const key of Object.keys(numberMap)) {
+      if (string.startsWith(key, i)) {
+        stringIndex = i;
+        str = numberMap[key];
+        break;
+      }
     }
+    if (stringIndex != -1) break;
+  }
 
-    if (str == 0) return num;
-    if (numberIndex < stringIndex) return num;
-    else return str;
+  if (str == 0) return num;
+  if (numberIndex < stringIndex) return num;
+  else return str;
 }
 
 function lastNumber(string) {
-    let numberIndex = -1;
-    let num = 0;
-    let stringIndex = -1;
-    let str = 0;
+  let numberIndex = -1;
+  let num = 0;
+  let stringIndex = -1;
+  let str = 0;
 
-    for (let i = string.length - 1; i >= 0; i--) {
-        if (isNumber(string[i])) {
-            numberIndex = i;
-            num = parseInt(string[i]);
-            break;
-        }
+  for (let i = string.length - 1; i >= 0; i--) {
+    if (isNumber(string[i])) {
+      numberIndex = i;
+      num = parseInt(string[i]);
+      break;
     }
+  }
 
-    for (let i = 0; i < string.length; i++) {
-        for (const key of Object.keys(numberMap)) {
-            if (string.startsWith(key, i)) {
-                stringIndex = i;
-                str = numberMap[key];
-            }
-        }
+  for (let i = 0; i < string.length; i++) {
+    for (const key of Object.keys(numberMap)) {
+      if (string.startsWith(key, i)) {
+        stringIndex = i;
+        str = numberMap[key];
+      }
     }
+  }
 
-    if (str == 0) return num;
-    if (numberIndex > stringIndex) return num;
-    else return str;
+  if (str == 0) return num;
+  if (numberIndex > stringIndex) return num;
+  else return str;
 }
 
 function calculate(string) {
-    let first = firstNumber(string);
-    let last = lastNumber(string);
-    return first * 10 + last;
+  let first = firstNumber(string);
+  let last = lastNumber(string);
+  return first * 10 + last;
 }
 
 let count = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    count += calculate(arr[i]);
-    //console.log(firstNumber(arr[i]), lastNumber(arr[i]));
+  count += calculate(arr[i]);
+  //console.log(firstNumber(arr[i]), lastNumber(arr[i]));
 }
 
 console.log(count);
--- 2023/day10.js
+++ 2023/day10.js
@@ -2,23 +2,23 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./2023/day10.txt', 'utf-8');
 const arr = input
-    .trim()
-    .split('\n')
-    .map(line => [...line.trim()]);
+  .trim()
+  .split('\n')
+  .map(line => [...line.trim()]);
 
 function findStart(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const line = arr[i];
-        const index = line.indexOf('S');
+  for (let i = 0; i < arr.length; i++) {
+    const line = arr[i];
+    const index = line.indexOf('S');
 
-        if (index !== -1) {
-            return { x: index, y: i };
-        }
+    if (index !== -1) {
+      return { x: index, y: i };
     }
+  }
 }
 
 function calculateFurthestPoint(arr) {
-    const start = findStart(arr);
+  const start = findStart(arr);
 }
 
 const steps = calculateFurthestPoint(arr);
